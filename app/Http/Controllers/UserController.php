a72ca6761870a06ab2ec993aff8f6765c630fb8d
Johnatan David Gersom <bengbeng9605@gmail.com>
2025-04-10 17:05:30 +0700

    js4-2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,17 +9,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller {
         public function index()
         {
-        $data = [
-                'level_id' => 2,
-                'username' => 'manager_tiga',
-                'nama' => 'Manager 3',
-                'password' => Hash::make('12345')
-            ];
-            UserModel::create($data);
-    
-            // Coba akses model UserModel
-            $user = UserModel::where('username', 'manager9')->firstOrFail();
-            return view('user', ['data' => $user]);
-    
+        $user = UserModel::where('level_id', 2)->count();
+        return view('user', ['data' => $user]);    
     }
 }
